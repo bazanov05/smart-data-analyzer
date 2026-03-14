c1a9ffac20ab347200f0063db7f2c915b80add1f
Alex <01210133@pw.edu.pl>
2026-03-14 18:08:37 +0100

    changed the logic of await - now it awaits for pandas to get data and event loop is not busy with it

--- app/api/transactions.py
+++ app/api/transactions.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
 import pandas as pd
-import io
 from app.services.analyzer import AML_System
+from fastapi.concurrency import run_in_threadpool
 
 
 app = FastAPI()
@@ -12,12 +12,11 @@ REQUIRED_COLUMNS = {'transaction_id', 'sender_id', 'receiver_id', 'amount', 'cou
 async def upload_a_csv_file(file: UploadFile = File(...)):
     if not file.filename.endswith('.csv'):
         raise HTTPException(status_code=415, detail="Unsupported media type")
-    content = await file.read()
-    df = pd.read_csv(io.BytesIO(content))
+    df = await run_in_threadpool(pd.read_csv, file.file)  # send it to thread so API is not blocked
     cols_in_df = set(df.columns)
     missing_cols = REQUIRED_COLUMNS - cols_in_df
     if len(missing_cols) != 0:
-        raise HTTPException(status_code=400, detail=missing_cols)
+        raise HTTPException(status_code=400, detail=list(missing_cols))
     aml = AML_System(df)
     structuring_attemps = aml.detect_structuring_attempts().fillna("None").to_dict(orient='records')
     unverified_originators = aml.identify_unverified_originators().fillna("None").to_dict(orient='records')
