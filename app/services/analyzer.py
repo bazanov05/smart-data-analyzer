069440a6e8e939efdfec7a16f485b0a4601ec1b5
Alex <01210133@pw.edu.pl>
2026-03-14 18:19:29 +0100

    added two new attributes to AML class - flexible limits, targer and buffer

--- app/services/analyzer.py
+++ app/services/analyzer.py
@@ -2,11 +2,20 @@ import pandas as pd
 
 
 class AML_System:
-    def __init__(self, transaction_ledger: pd.DataFrame):
+    def _is_value_positive(self, value):
+        return value > 0
+
+    def __init__(self, transaction_ledger: pd.DataFrame, target_limit=10000, buffer=1000):
         self._df = transaction_ledger
+        if not self._is_value_positive(target_limit) or not self._is_value_positive(buffer):
+            raise ValueError("Limits and buffer should be positive values")
+        if buffer >= target_limit:
+            raise ValueError("Buffer should be less than target limit")
+        self._target_limit = target_limit
+        self._buffer = buffer
 
     def detect_structuring_attempts(self):
-        return self._df.loc[((self._df.amount >= 9000) & (self._df.amount <= 9999))]
+        return self._df.loc[((self._df.amount >= self._target_limit - self._buffer) & (self._df.amount < self._target_limit))]
 
     def identify_unverified_originators(self):
         anonymous_mask = self._df.isnull()
