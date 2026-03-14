486cac0e8329df212283cefa5d64452d88c8f21f
Alex <01210133@pw.edu.pl>
2026-03-14 18:00:28 +0100

    added a validation of missing cols and raise HTTP error

--- app/api/transactions.py
+++ app/api/transactions.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 import pandas as pd
 import io
 from app.services.analyzer import AML_System
 
 
 app = FastAPI()
+REQUIRED_COLUMNS = {'transaction_id', 'sender_id', 'receiver_id', 'amount', 'country', 'type'}
 
 
 @app.post("/update")
 async def upload_a_csv_file(file: UploadFile = File(...)):
     if not file.filename.endswith('.csv'):
-        return {"Error": "not csv format"}
+        raise HTTPException(status_code=415, detail="Unsupported media type")
     content = await file.read()
     df = pd.read_csv(io.BytesIO(content))
+    cols_in_df = set(df.columns)
+    missing_cols = REQUIRED_COLUMNS - cols_in_df
+    if len(missing_cols) != 0:
+        raise HTTPException(status_code=400, detail=missing_cols)
     aml = AML_System(df)
     structuring_attemps = aml.detect_structuring_attempts().fillna("None").to_dict(orient='records')
     unverified_originators = aml.identify_unverified_originators().fillna("None").to_dict(orient='records')
