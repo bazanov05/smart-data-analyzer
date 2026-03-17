ea21c0f9754f94fcb60c7581544403d9c1df2efd
Alex <01210133@pw.edu.pl>
2026-03-17 19:29:14 +0100

    added get_db() method

--- app/db/database.py
+++ app/db/database.py
@@ -4,14 +4,31 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 SQLALCHEMY_DATABASE_URL = "sqlite:///./aml_database.db"
 
 
-#  creating a physical connection to my SQLite database providing flexible connection
+#  gateaway between Python and db
+#  doesn't connect immediately, only when a session requests it
+#  check_some_thread = False allows diff threads to share the same engine
+#  otherwise each engine requires its own thread
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 
 #  directs the session to our data base
 #  i want to commit and flush on my own
+#  its the real conversation with our db
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-#  basic class, my future class will inherit from it to be automatically added to db
+#  basic class, AmlReport inherits from it to be registered that it exists
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()  # opens a live conversation with db
+    try:
+        yield db  # hands session to route from FastAPI and waits until its done
+    finally:  # finally means line runs no matter what
+        db.close()  # closes the session
+
+
+# reads Base registry and builds all tables in the .db file if they dont exist yet
+# in base register there are only classes which inherit from Base
+Base.metadata.create_all(bind=engine)
