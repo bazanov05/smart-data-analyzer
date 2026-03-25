bfc6ddd75140b3c77ec7440df7581b1f65edf900
Alex <01210133@pw.edu.pl>
2026-03-25 18:38:11 +0100

    add GET routes for all four report types

--- app/api/transactions.py
+++ app/api/transactions.py
@@ -3,13 +3,36 @@ import pandas as pd
 from app.services.analyzer import AML_System
 from fastapi.concurrency import run_in_threadpool  # for pd.read_csv()
 from app.db.database import get_db
-from app.db.repository import create_report, get_report_by_id
-from app.db.schemas import AmlReportResponse
+from app.db.repository import(
+    get_structuring_attempt_report_by_id,
+    get_geographical_inflow_report_by_id,
+    get_high_velocity_transfer_report_by_id,
+    get_unverified_originator_report_by_id
+)
+from app.db.schemas import(
+    StructuringAttemptResponse,
+    UnverifiedOriginatorResponse,
+    GeographicalInflowResponse,
+    HighVelocityTransferResponse
+)
 from sqlalchemy.orm import Session
 
 
 app = FastAPI()
 REQUIRED_COLUMNS = {'transaction_id', 'sender_id', 'receiver_id', 'amount', 'country', 'type', 'timestamp'}
+
+
+def _save_report_with_timestamp(db, reports: list):
+    """
+    Saves reports with timestamp in db
+    """
+    saved = []
+    for report in reports:
+        if "timestamp" in report:
+            report["timestamp"] = report['timestamp'].to_pydatetime()
+            new_report = create_report(db, report)
+            saved.append(new_report)
+    return saved
 
 
 @app.post("/update")
@@ -33,39 +56,49 @@ async def upload_a_csv_file(file: UploadFile = File(...), db=Depends(get_db)):
     geographic_inflows = aml.aggregate_geographic_inflow().to_dict()
     high_velocity_transfers = aml.detect_high_velocity_transfers().to_dict(orient="records")
 
-    saved_struct_attempt_reports = []
-    for structuring_attempt in structuring_attemps:
-        new_report = create_report(db, structuring_attempt)
-        saved_struct_attempt_reports.append(new_report)
+    saved_struct_attempt_reports = _save_report_with_timestamp(db, structuring_attemps)
 
-    saved_unver_org_reports = []
-    for unverified_originator in unverified_originators:
-        new_report = create_report(db, unverified_originator)
-        saved_unver_org_reports.append(new_report)
+    saved_unver_org_reports = _save_report_with_timestamp(db, unverified_originators)
 
-    saved_geo_inflow_reports = []
-    for geographic_inflow in geographic_inflows:
-        new_report = create_report(db, geographic_inflow)
-        saved_geo_inflow_reports.append(new_report)
-
-    saved_velocity_transfers_reports = []
-    for high_velocity_transfer in high_velocity_transfers:
-        new_report = create_report(db, high_velocity_transfer)
-        saved_velocity_transfers_reports.append(new_report)
+    saved_velocity_transfers_reports = _save_report_with_timestamp(db, high_velocity_transfers)
 
     result = {
         "structuring_attempts": saved_struct_attempt_reports,
         "unverified_originators": saved_unver_org_reports,
-        "geographi_inflows": saved_geo_inflow_reports,
+        "geographi_inflows": geographic_inflows,
         "high_velocity_transfers": saved_velocity_transfers_reports
 
     }
     return result
 
 
-@app.get("/reports/{report_id}", response_model=AmlReportResponse)
-def get_report(report_id: int, db: Session = Depends(get_db)):
-    report = get_report_by_id(db, report_id)
+@app.get("/reports/structuring_attempt/{report_id}", response_model=StructuringAttemptResponse)
+def get_structuring_attempt(report_id: int, db: Session = Depends(get_db)):
+    report = get_structuring_attempt_report_by_id(db, report_id)
     if report is None:
-        raise HTTPException(status_code=404, detail="Report not found")
+        raise HTTPException(status_code=404, detail="Report with this id does not exist")
+    return report
+
+
+@app.get("/reports/unverified_originator/{report_id}", response_model=UnverifiedOriginatorResponse)
+def get_unverified_originator(report_id: int, db: Session = Depends(get_db)):
+    report = get_unverified_originator_report_by_id(db, report_id)
+    if report is None:
+        raise HTTPException(status_code=404, detail="Report with this id does not exist")
+    return report
+
+
+@app.get("/reports/geographical_inflow/{report_id}", response_model=GeographicalInflowResponse)
+def get_geographical_inflow(report_id: int, db: Session = Depends(get_db)):
+    report = get_geographical_inflow_report_by_id(db, report_id)
+    if report is None:
+        raise HTTPException(status_code=404, detail="Report with this id does not exist")
+    return report
+
+
+@app.get("/reports/high_velocity_transfer/{report_id}", response_model=HighVelocityTransferResponse)
+def get_high_velocity_transfer(report_id: int, db: Session = Depends(get_db)):
+    report = get_high_velocity_transfer_report_by_id(db, report_id)
+    if report is None:
+        raise HTTPException(status_code=404, detail="Report with this id does not exist")
     return report
