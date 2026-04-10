ef0baf6a947636039e5ab36db04dcfb64e3f0cb6
Alex <01210133@pw.edu.pl>
2026-04-10 17:38:19 +0200

    implement create_aml_agent() method which returns engine with llm agent, promt and tools

--- app/services/ai_agent.py
+++ app/services/ai_agent.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel, Field
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import SystemMessage
+from sqlalchemy.orm import Session
+from langchain_anthropic import ChatAnthropic
+from langchain_classic.agents import AgentExecutor, create_tool_calling_agent
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from tools import (
+    get_structuring_tool
+)
 
 
 # defines the shape of the ai answer, makes it more structured
@@ -20,3 +27,38 @@ system_promt = (
 )
 
 system_message = SystemMessage(content=system_promt)
+
+
+def create_aml_agent(db_session: Session):
+    """
+    Creates a fully ready-to-use Engine,
+    which consists of llm agent, tools, and conversational framework.
+    Provides a safe db Session to our outer fuctions in tools
+    and gets the tools.
+    """
+
+    # temprature = 0 removes randomness
+    model_object = ChatAnthropic(model="claude-3-5-sonnet-latest", temperature=0)
+    my_tools = []
+
+    structuring_tool = get_structuring_tool(db_session)
+    my_tools.append(structuring_tool)
+
+    # creating the strcit conversational framework that AI must follow
+    prompt = ChatPromptTemplate.from_messages([
+        # 1. The Persona & Rules (with explicit JSON schema instruction)
+        ("system", system_promt + "\nEnsure your final answer is strictly in JSON format matching the defined AMLAnalysisSchema."),
+
+        # 2. The User Input
+        ("human", "{input}"),
+
+        # 3. The Required Working Space
+        MessagesPlaceholder(variable_name="agent_scratchpad"),
+    ])
+
+    # binds llm, tools and prompt into single logical unit
+    agent = create_tool_calling_agent(model_object, my_tools, prompt)
+
+    agent_executor = AgentExecutor(agent=agent, tools=my_tools, verbose=True)
+
+    return agent_executor
